de8f816ecd0289e1f0636d8f848446d558b8e051
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-04 18:50:19 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app import app
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path, mimetype='application/javascript')
 
 app.secret_key = 'cooperwashere'
 if __name__ == '__main__':
