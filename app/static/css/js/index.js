377b5346feb242def58977ff7cb7fcfb5331d9b6
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-05 09:54:03 -0400

    Update index.js

--- app/static/css/js/index.js
+++ app/static/css/js/index.js
@@ -2,8 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-        // Your form handling logic goes here
+        event.preventDefault();
         console.log('Form submitted');
     });
 });
