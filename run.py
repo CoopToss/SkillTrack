f728c72de4f9e0717a2af3fb2e3ab4cc0ff23521
Cooper Martin <156128377+CoopToss@users.noreply.github.com>
2024-06-05 09:47:56 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
-from app import app
 from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/js/<path:path>')
+@app.route('/static/js/<path:path>')
 def send_js(path):
     return send_from_directory('static/js', path, mimetype='application/javascript')
 
